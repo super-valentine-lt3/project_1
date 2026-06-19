cbe4b35eeeba919f9d4fcc997512da5ecf065c70
Super Valentine <melval33@proton.me>
2026-06-19 12:31:39 -0400

    Added moving character code with aseperite stuff

--- go.mod
+++ go.mod
@@ -5,15 +5,17 @@ go 1.26.4
 require (
 	github.com/hajimehoshi/ebiten/v2 v2.9.9
 	github.com/lafriks/go-tiled v0.14.0
+	github.com/solarlune/goaseprite v0.0.0-20241229161216-14dfd2cced08
 )
 
 require (
-	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/ebitengine/gomobile v0.0.0-20250923094054-ea854a63cce1 // indirect
 	github.com/ebitengine/hideconsole v1.0.0 // indirect
 	github.com/ebitengine/purego v0.9.0 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
-	golang.org/x/image v0.31.0 // indirect
+	github.com/tidwall/gjson v1.10.2 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"image"
+
+	"github.com/solarlune/goaseprite"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -14,6 +17,11 @@ import (
 const mapPath = "assets/map_1.tmx" // Path to your Tiled Map.
 var img *ebiten.Image
 var gameMap *tiled.Map 
+var speed = 2.0
+
+const CharacterSpriteFile = "character_base_16x16.json"
+const CharacterSpriteDirectory = "./assets"
+const CharacterSpriteStartAnim = "idle_down "
 
 func init() {
 	var err error
@@ -73,18 +81,159 @@ func DrawTiledLayer(screen *ebiten.Image, name string) {
 	    }
 	}	
 }
+type Direction int
 
-type Game struct{}
+const (
+    Up Direction = iota
+    Down 
+    Left 
+    Right 
+)
 
-func (g *Game) Update() error {
+var CurrentDirection = Down 
+
+type Game struct{
+	Char *Character 
+}
+
+type Character struct {
+	Sprite *SpriteAnim 
+	PosX float64
+	PosY float64	
+}
+
+type SpriteAnim struct {
+	Sprite    *goaseprite.File
+	AsePlayer *goaseprite.Player
+	Img       *ebiten.Image	
+}
+
+func (sprite *SpriteAnim) Play(animation string) {
+	sprite.AsePlayer.Play(animation)
+}
+func (sprite *SpriteAnim) Update(delta float32) {
+	sprite.AsePlayer.Update(delta)
+}
+
+func NewSpriteAnim(
+	file string, directory string, start_anim string) *SpriteAnim {
+	//sprite, err := goaseprite.Open("character_base_16x16.json", os.DirFS("./assets"))
+	
+	sprite, err := goaseprite.Open(file, os.DirFS(directory))
+
+	if err != nil {
+		panic(err)
+	}
+	spriteAnim := &SpriteAnim {
+		Sprite: sprite, 
+	}
+
+	spriteAnim.AsePlayer = spriteAnim.Sprite.CreatePlayer()
+	img, _, err := ebitenutil.NewImageFromFile(spriteAnim.Sprite.ImagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	// game.Sprite.PlaySpeed = 2
+
+	spriteAnim.Img = img
+	spriteAnim.AsePlayer.Play(start_anim)
+	return spriteAnim
+}
+
+func (sprite *SpriteAnim) Draw(Screen *ebiten.Image, PosX float64,  PosY float64) {
+	opts := &ebiten.DrawImageOptions{}
+    opts.GeoM.Translate(
+        float64(PosX),
+        float64(PosY),
+    )
+	sub := sprite.Img.SubImage(image.Rect(sprite.AsePlayer.CurrentFrameCoords()))
+
+	Screen.DrawImage(sub.(*ebiten.Image), opts)
+}
+
+func NewCharacter(obj *tiled.Object, 
+			sprite_file string, 
+			sprite_directory string, 
+			default_anim string ) *Character{
+	character := &Character{}
+	// sprite, err := goaseprite.Open("character_base_16x16.json", os.DirFS("./assets"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// character := &Character{
+	// 	Sprite: sprite,
+	// }
+
+	// character.AsePlayer = character.Sprite.CreatePlayer()
+	// img, _, err := ebitenutil.NewImageFromFile(character.Sprite.ImagePath)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// // game.Sprite.PlaySpeed = 2
+
+	// character.Img = img
+	// character.AsePlayer.Play("idle_down")
+	// character.PosX = obj.X 
+	// character.PosY = obj.Y 
+	spriteAnim := NewSpriteAnim(sprite_file, sprite_directory, default_anim)
+	character.Sprite = spriteAnim
+	character.PosX = obj.X 
+	character.PosY = obj.Y 
+	return character
+}
+
+func (c *Character) Draw(screen *ebiten.Image) {
+	c.Sprite.Draw(screen, c.PosX, c.PosY)
+}
+
+func (c *Character) Update() {
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		c.Sprite.Play("walk_up")
+		c.PosY = c.PosY - speed 
+		CurrentDirection = Up 
+	} else if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		c.Sprite.Play("walk_down")
+		c.PosY = c.PosY + speed 
+		CurrentDirection = Down 
+	} else if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		c.Sprite.Play("walk_left")
+		c.PosX = c.PosX - speed 
+		CurrentDirection = Left 
+	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		c.Sprite.Play("walk_right")
+		c.PosX = c.PosX + speed 
+		CurrentDirection = Right 
+	} else {
+	    switch CurrentDirection {
+	    case Up:
+	       c.Sprite.Play("idle_up")
+	    case Down:
+	       c.Sprite.Play("idle_down")
+	    case Left:
+	       c.Sprite.Play("idle_left")
+	    case Right:
+	       c.Sprite.Play("idle_right")	       
+	    default:
+	       c.Sprite.Play("idle_down")
+	    }
+	}
+
+	c.Sprite.Update(float32(1.0 / 60.0))
+
+}
+
+func (game *Game) Update() error {
+	game.Char.Update()
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
+func (game *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")	
 	DrawTiledLayer(screen, "base")
 	DrawTiledLayer(screen, "over")
-
+	game.Char.Draw(screen) 
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
@@ -94,10 +243,20 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 func main() {
 	objectsLayer := GetObjectGroup("objects") 	// Search for Object Layer called "objects"
 	object := GetObjectFromObjectLayer(objectsLayer, "PlayerStart")
+	fmt.Println(object)
 
+
+	Char := NewCharacter(object, 
+			CharacterSpriteFile, 
+			CharacterSpriteDirectory, 
+			CharacterSpriteStartAnim)
+
+	game := &Game {
+		Char: Char, 
+	}
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
 }
